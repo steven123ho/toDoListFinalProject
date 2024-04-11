b69ed280e6fe850ff6d164f301057e1bd2d95b6c
steven123ho <steven123ho@gmail.com>
2024-04-11 16:42:45 -0400

    UI Finished and View Controller files created. No functionality

---
+++ TodoListFinalProject/Constants.swift
@@ -0,0 +1,8 @@
+//
+//  Constants.swift
+//  TodoListFinalProject
+//
+//  Created by user246123 on 4/11/24.
+//
+
+import Foundation
---
+++ TodoListFinalProject/DateViewController.swift
@@ -0,0 +1,29 @@
+//
+//  DateViewController.swift
+//  TodoListFinalProject
+//
+//  Created by user246123 on 4/11/24.
+//
+
+import UIKit
+
+class DateViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
