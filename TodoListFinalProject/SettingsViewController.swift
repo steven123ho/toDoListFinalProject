38e459295b1e852730e07ce47354d839bfb1ebe4
Tan Mai <tanmai1126@gmail.com>
2024-04-19 14:00:51 -0400

    SettingView COntroller(delegate methods)  and App delegate

--- TodoListFinalProject/SettingsViewController.swift
+++ TodoListFinalProject/SettingsViewController.swift
@@ -7,14 +7,51 @@
 
 import UIKit
 
-class SettingsViewController: UIViewController {
+class SettingsViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
 
+    
+    @IBOutlet weak var pckSortField: UIPickerView!
+    @IBOutlet weak var swAscending: UISwitch!
+    
+    let sortOrderItems: Array<String> = ["subject", "priority", "dueDate"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        pckSortField.dataSource = self;
+        pckSortField.delegate = self;
+    }
+    @IBAction func sortDirectionChanged(_ sender: Any) {
+        let settings = UserDefaults.standard
+        settings.set(swAscending.isOn, forKey: Constants.kSortDirectionAscending)
+        settings.synchronize()
     }
     
+    // MARK: UIPickerViewDelegate Methods
+    
+    // returns the number of 'columns' to display
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    // Returns the number of rows in the picker
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return sortOrderItems.count
+    }
+    
+    // Sets the value that is shown for each row in the picker
+    func pickerView (_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return sortOrderItems[row]
+    }
+    
+    //If the user chooses from the pickerview, it calls this function
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        let sortField = sortOrderItems[row]
+        let settings = UserDefaults.standard
+        settings.set(sortField, forKey: Constants.kSortField)
+        settings.synchronize()
+    }
    //test commit
     
 }
